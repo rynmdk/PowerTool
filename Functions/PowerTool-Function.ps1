69e45ceae27f926cf5b0b1625a846615814784d0
Erik JA <OgJAkFy8@users.noreply.github.com>
2019-11-21 23:47:55 -0500

    Combined Functions

--- Functions/PowerTool-Function.ps1
+++ Functions/PowerTool-Function.ps1
@@ -1,259 +1,124 @@
-﻿<#
+﻿#requires -Version 3.0
 
-    .NAME
-        PowerTool-Functions
 
-    .VERSION
-        0.1
+Set-Location -Path $PSScriptRoot
 
-    .DESCRIPTION
-        Functions required for ..\PowerTool.ps1
 
-    .LICENSE
-        MIT License
-
-#>
-
-Set-Location $PSScriptRoot
+. .\edit-ptlog.ps1 
+. .\AdminTools.ps1 
 
 # Main Functions
+function Connect-PTworkstation 
+{
+  # Connect to remote workstation
 
-function PT-Connect {
+  Edit-PTlog -Clear
+  Set-PtRemoteTools -SetState Disable
 
-    # Connect to remote workstation
+  # Set hostname up as a global variable based on the hostname entry
+  $global:hostname = $txbHostname.text
 
-    PT-ClearLog
-    PT-DisableRemoteTools
-
-    # Set hostname up as a global variable based on the hostname entry
-    $global:hostname = $txbHostname.text
-
-    if ($hostname -eq "") {
+  if ($hostname -eq '') 
+  {
+    Edit-PTlog -Write -logString 'No Computer Name or IP Address entered'
+  }
+  else 
+  {
+    $TestConnection = Test-Connection -ComputerName $hostname -Count 1 -BufferSize 16 -ErrorAction SilentlyContinue
     
-        PT-LogWrite "No Computer Name or IP Address entered"
-
-    } else {
-    
-        $TestConnection = Test-Connection -ComputerName $hostname -Count 1 -BufferSize 16 -ErrorAction SilentlyContinue
-    
-        if (!($TestConnection)) {
-
-            PT-LogWrite "Unable to Connect to $hostname"
-
-        } else {
-
-            try {
-    
-                # Get target hostname
-                $MachineName = Get-WmiObject -Class win32_computersystem -ComputerName $hostname
-         
-                # Compare entered hostname to target hostname to ensure no DNS issue
-                if ((!($hostname -like "*.*")) -and ($MachineName.name -ne $hostname)) {
-
-                    # Stop processing as DNS issue occured
-                    PT-ClearLog
-                    PT-DisableRemoteTools
-                    PT-LogWrite "DNS issue encountered connecting to $hostname"    
-                                   
-                } else {
-     
-                    PT-ClearLog
-                    PT-EnableRemoteTools
-                    PT-LogWrite "Successfully connected to $hostname"
-                
-                }
-
-            } catch {
-
-                PT-ClearLog
-                PT-LogWrite "Unable to connect to $hostname"
-
-            }
-
-        }
-
+    if (!($TestConnection)) 
+    {
+      Edit-PTlog -Write -logString ('Unable to Connect to {0}' -f $hostname)
     }
-
+    else 
+    {
+      try 
+      {
+        # Get target hostname
+        $MachineName = Get-WmiObject -Class win32_computersystem -ComputerName $hostname
+         
+        # Compare entered hostname to target hostname to ensure no DNS issue
+        if ((!($hostname -like '*.*')) -and ($MachineName.name -ne $hostname)) 
+        {
+          # Stop processing as DNS issue occured
+          Edit-PTlog -Clear
+          Set-PtRemoteTools -SetState Disable
+          Edit-PTlog -Write -logString ('DNS issue encountered connecting to {0}' -f $hostname)
+        }
+        else 
+        {
+          Edit-PTlog -Clear
+          Set-PtRemoteTools -SetState Enable
+          Edit-PTlog -Write -logString ('Successfully connected to {0}' -f $hostname)
+        }
+      }
+      catch 
+      {
+        Edit-PTlog -Clear
+        Edit-PTlog -Write -logString ('Unable to connect to {0}' -f $hostname)
+      }
+    }
+  }
 }
 
-function PT-ClearLog {
-
-    # Clear PowerTool Log
-
-    $txbLogging.clear()
-
+function Clear-PTlog 
+{
+  # Clear PowerTool Log
+  Edit-PTlog -Clear
 }
 
-function PT-CopyClipboard {
+
+function Copy-PTLogs 
+{
+  # Function to copy logs to clipboard
+  # Copy-Clipboard required
+  
+  Edit-PTlog -Copy
+  Edit-PTlog -Write -logString 'Log copied to clipboard'
+}
+
+function Write-PTlog 
+{
+  # Add log to PowerTool log screen
+  Write-Error -Message 'This is still being used somewhere'
+}
+
+function PT-LineSpacer 
+{
+  # Add linespacer to logs - for formatting only
+  # $txbLogging.Appendtext("{0}`r`n" -f $PtLine)
+  Return
+}
+
+function Set-PtRemoteTools 
+{
+  [CmdletBinding()]
+  param(
+    [Parameter(Mandatory = $true, ValueFromPipeline = $true,
+    ValueFromPipelineByPropertyName = $true)]
+    [ValidateSet('Enable','Disable')]
+    [string]$SetState
+  )
+  
+  if($SetState -eq 'Enable')
+  {
+    $ToolState = $true
+  }
+  if($SetState -eq 'Disable')
+  {
+    $ToolState = $false
+  }
     
-    # Function to copy text to clipboard
-    # Used with CopyLogs function - DO NOT DELETE
-
-    param([string]$text)
-	Add-Type -AssemblyName System.Windows.Forms
-	$tb = New-Object System.Windows.Forms.TextBox
-	$tb.Multiline = $true
-	$tb.Text = $text
-	$tb.SelectAll()
-	$tb.Copy()	
-
+  $computerInformationDropdown.Visible = $ToolState
+  $fixesDropdown.Visible = $ToolState
+  $appvDropdown.Visible = $ToolState
+  $applicationsDropdown.Visible = $ToolState
+  
+  $tools_remote_Explorer.Enabled = $ToolState
+  $tools_remote_Services.Enabled = $ToolState
+  $tools_remote_Ping.Enabled = $ToolState
 }
 
-function PT-CopyLogs {
-    
-    # Function to copy logs to clipboard
-    # Copy-Clipboard required
-
-    $Text = $txb_logs.Text
-    PT-CopyClipboard -text $Text
-    PT-LogWrite "Log copied to clipboard"
-
-}
-
-function PT-LogWrite {
-
-    # Add log to PowerTool log screen
-
-    Param ([string]$logString)
-    $now = Get-Date -Format g
-    $txbLogging.Appendtext("$now - $logString `r`n")
-
-
-    # Create external logs
-    New-Item -Path ..\ -Name "Logs" -ItemType Directory -ErrorAction Ignore
-    $date = (Get-Date).ToString("dd-MM-yyyy")
-    $now = '{0:HH:mm:ss}' -f (get-date)
-    $Log = "..\Logs\"+$env:USERNAME+"-"+$date+"-"+$hostname+".log"
-    Add-Content $log -Value "$now - $logstring" -Force
-
-}
-
-function PT-LineSpacer {
-
-    # Add linespacer to logs - for formatting only
-
-    $txbLogging.Appendtext("-------------------------------------------------------------------------------------------------------------`r`n")
-
-}
-
-function PT-EnableRemoteTools {
-
-    # Enable Remote Tools & Dropdowns
-
-    $computerInformationDropdown.Visible = $True
-    $fixesDropdown.Visible = $True
-    $appvDropdown.Visible = $True
-    $applicationsDropdown.Visible = $True
-
-    $tools_remote_Explorer.Enabled = $True
-    $tools_remote_Services.Enabled = $True
-    $tools_remote_Ping.Enabled = $True
-
-}
-
-function PT-DisableRemoteTools {
-
-    # Disable Remote Tools & Dropdowns
-
-    $computerInformationDropdown.Visible = $False
-    $fixesDropdown.Visible = $False
-    $appvDropdown.Visible = $False
-    $applicationsDropdown.Visible = $False
-
-    $tools_remote_Explorer.Enabled = $False
-    $tools_remote_Services.Enabled = $False
-    $tools_remote_Ping.Enabled = $False
-
-}
-
-# Admin Tools
-function PT-FileExplorer {
-
-    Start-Process explorer.exe
-    PT-LogWrite "File Explorer launched"
-    PT-LineSpacer
-
-}
-
-function PT-Services {
-
-    Start-Process services.msc
-    PT-LogWrite "Services.msc launched"
-    PT-LineSpacer
-
-}
-
-function PT-Regedit {
-
-    Start-Process regedit
-    PT-LogWrite "Regedit launched"
-    PT-LineSpacer
-
-}
-
-function PT-PowerShell {
-
-    Start-Process powershell
-    PT-LogWrite "PowerShell launched"
-    PT-LineSpacer
-
-}
-
-function PT-CMD {
-
-    Start-Process cmd
-    PT-LogWrite "CMD launched"
-    PT-LineSpacer
-
-}
-
-function PT-DeviceManager {
-
-    Start-Process devmgmt
-    PT-LogWrite "Device Manager launched"
-    PT-LineSpacer
-
-}
-
-function PT-EventViewer {
-
-    Start-Process eventvwr
-    PT-LogWrite "Event Viewer launched"
-    PT-LineSpacer
-
-}
-
-function PT-SystemSettings {
-
-    Start-Process sysdm.cpl
-    PT-LogWrite "Advanced System Settings launched"
-    PT-LineSpacer
-
-}
-
-function PT-RemoteExplorer {
-
-    ii \\$hostname\c$\
-    PT-LogWrite "File Explorer launched for $hostname"
-    PT-LineSpacer
-
-}
-
-function PT-RemoteServices {
-
-    services.msc /computer=$hostname
-    PT-LogWrite "Services.msc launched for $hostname"
-    PT-LineSpacer
-
-}
-
-function PT-RemotePing {
-
-    Start-Process ping.exe -ArgumentList "$hostname -n 300"
-    PT-LogWrite "Ping launched for $hostname"
-    PT-LineSpacer
-
-}
 
 # Account Information
 
